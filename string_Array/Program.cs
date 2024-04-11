0d19fb5e91ce5a1986b4f458690f90ff992aa1da
tilak-giri <tilakgiri01@gmail.com>
2024-04-11 12:07:55 +0545

    second commit

---
+++ string_Array/Program.cs
@@ -0,0 +1,7 @@
+﻿// Reverse Array of Strings: Write a method to reverse an array of strings in C#.
+
+string[] names = new string[] { "tilak", "bibek", "binod", "anil"};
+Array.Reverse(names);
+foreach (string name in names){
+    Console.WriteLine(name);
+}
