86e62d5f64d0afe0190577d3b003f2ae98f1537b
tilak-giri <tilakgiri01@gmail.com>
2024-04-23 22:55:46 +0545

    the concept of Class

---
+++ class/Program.cs
@@ -0,0 +1,21 @@
+﻿//this is my main class "Program"
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Message.Hello();
+        Message.By();
+    }
+}
+//here i have created a class named "Message" with two methods.
+public static class Message
+{
+    public static void Hello()
+    {
+        Console.WriteLine("Hello Good Morning!");
+    }
+    public static void By()
+    {
+        Console.WriteLine("Tata!!!");
+    }
+}
