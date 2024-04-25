63ae72475e05bde4cbac7a4e79b07ccb856469e1
tilak-giri <tilakgiri01@gmail.com>
2024-04-25 22:31:16 +0545

    learning objects

---
+++ objects/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Human manxe = new Human();
+        manxe.name = "Tilak Giri";
+        manxe.age = 21;
+
+        manxe.nam();
+        manxe.umer();
+    }
+}
+class Human
+{
+    public string name;
+    public int age;
+
+    public void nam()
+    {
+        Console.WriteLine($"My name is : {name}");
+    }
+    public void umer()
+    {
+        System.Console.WriteLine($"My age is : {age}");
+    }
+}
