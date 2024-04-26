baf3ba6152861d85cc67b3dd872819ae0ea676b2
tilak-giri <tilakgiri01@gmail.com>
2024-04-27 00:15:11 +0545

    learning static modifier

---
+++ staticModifier/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program{
+    public static void Main(string[] args)
+    {
+        Car car1 = new Car("Ferrari");
+        Car car2 = new Car("Supra");
+        Car car3 = new Car("Ford");
+        Console.WriteLine($"Numbers of cars in the race are : {Car.carCounter}");
+        Car.Drive();
+    }
+}
+class Car
+{
+    public string model;
+    public static int carCounter;
+    public Car(string model)
+    {
+        this.model = model;
+        carCounter += 1;
+    }
+    public static void Drive()
+    {
+        Console.WriteLine($"The race has begun!!!");
+    }
+}
