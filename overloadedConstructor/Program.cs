6eadee25d2ddf24e57b748cd5045aaf73383abc1
tilak-giri <tilakgiri01@gmail.com>
2024-04-29 08:06:11 +0545

    constructor overloading

---
+++ overloadedConstructor/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program{
+    public static void Main(string[] args)
+    {
+        Momo momo1 = new Momo("chicken", "johl", "chatni");
+        Momo momo2 = new Momo("veg", "fry");
+        Momo momo3 = new Momo("chicken");
+    }
+}
+class Momo
+{
+    string item;
+    string type;
+    string extra;
+    public Momo(string item, string type, string extra)
+    {
+        this.item = item;
+        this.type = type;
+        this.extra = extra;
+    }
+    public Momo(string item, string type)
+    {
+        this.item = item;
+        this.type = type;
+    }
+    public Momo(string item)
+    {
+        this.item = item;
+    }
+}
