2331e1c151661625b66dc454df1e6fbe54f2ea5e
tilak-giri <tilakgiri01@gmail.com>
2024-05-06 22:27:36 +0545

    inheritence

---
+++ inheritence/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bike bike = new Bike();
+        Ship ship = new Ship();
+
+        Console.WriteLine(car.speed);
+        Console.WriteLine(car.wheels);
+        car.run();
+
+        Console.WriteLine(bike.speed);
+        Console.WriteLine(bike.wheels);
+        bike.run();
+
+        Console.WriteLine(ship.speed);
+        Console.WriteLine(ship.wheels);
+        ship.run();
+    }
+    class Vehicle
+    {
+        public int speed = 0;
+
+        public void run()
+        {
+            Console.WriteLine("The vehicle is moving now...");
+        }
+    }
+    class Car : Vehicle{
+        public int wheels = 4;
+    }
+    class Bike : Vehicle{
+        public int wheels = 2;
+    }
+    class Ship : Vehicle{
+        public int wheels = 0;
+    }
+}
