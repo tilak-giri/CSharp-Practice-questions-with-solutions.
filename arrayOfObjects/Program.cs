415c7edf3b8e1ee746e6693531b11fcd829bfa03
tilak-giri <tilakgiri01@gmail.com>
2024-05-07 22:51:18 +0545

    array of objects

---
+++ arrayOfObjects/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program{
+    public static void Main(string[] args){
+        Car[] garrage = {new Car("Corvette"), new Car("Ferrari"), new Car("Lamborghini")};
+
+        foreach (Car car in garrage)
+        {
+            Console.WriteLine(car.model);
+        }
+    }
+    class Car{
+        public string model;
+        public Car(string model){
+            this.model = model;
+        }
+    }
+}
