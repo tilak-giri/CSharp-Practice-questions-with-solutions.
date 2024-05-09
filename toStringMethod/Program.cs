ec58eefd6ff3fb7922436dff9a45a2fa53df2792
tilak-giri <tilakgiri01@gmail.com>
2024-05-09 22:54:28 +0545

    toString method

---
+++ toStringMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MyNameSpace{
+    class Program{
+        static void Main(string[] args){
+            Vehicle car = new Vehicle("Ford", "Mustang", 2022, "Red");
+            Console.WriteLine(car);
+        }
+    }
+    class Vehicle{
+        public string make;
+        public string model;
+        public int year;
+        public string color;
+        public Vehicle(string make, string model, int year, string color){
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+        public override string ToString() {
+            return "The " + make + " " + model + " from " + year + " in " + color;
+        }
+    }
+}
