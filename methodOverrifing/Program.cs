50fd51415b1d7c7cbfe8f68bfd5c5708e55b667a
tilak-giri <tilakgiri01@gmail.com>
2024-05-09 22:44:19 +0545

    method overriding

---
+++ methodOverrifing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MyProgram{
+    class Program{
+    public static void Main(string[] args){
+        Car car = new Car();
+        Bike bike = new Bike();
+
+        car.wheels();
+        bike.wheels();
+    }
+}
+class Vehicle{
+    public virtual void wheels(){
+        Console.WriteLine("The vehicle have 4 wheels...");
+    }
+}
+class Car : Vehicle{
+}
+class Bike : Vehicle{
+    public override void wheels(){
+        Console.WriteLine("The vehicle have 2 wheels...");
+    }
+}
+}
