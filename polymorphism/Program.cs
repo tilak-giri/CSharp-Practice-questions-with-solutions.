36af6e5c8a0574abb53e773581e39be845b67899
tilak-giri <tilakgiri01@gmail.com>
2024-05-10 23:38:27 +0545

    polimorphism

---
+++ polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MyNameSpace{
+class Program{
+    public static void Main(string[] args){
+        Car car = new Car();
+        Bike bike = new Bike();
+        Truck truck = new Truck();
+
+        Vehicle[] vehicle = {car, bike, truck};
+
+        foreach (Vehicle v in vehicle){
+            v.Go();
+        }
+    }
+}
+class Vehicle{
+    public virtual void Go(){
+        Console.WriteLine("Let's gooooooooooo");
+    }
+}
+class Car : Vehicle{
+        public override void Go()
+        {
+            Console.WriteLine("The Car is moving");
+        }
+    }
+class Bike : Vehicle{
+        public override void Go()
+        {
+            Console.WriteLine("The Bike is moving");
+        }
+}
+class Truck : Vehicle{
+        public override void Go()
+        {
+            Console.WriteLine("The Truck is moving");
+        }
+}
+}
