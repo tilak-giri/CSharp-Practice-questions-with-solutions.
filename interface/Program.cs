671bda830e450ccdb0fe65988f1d6998a41496f9
tilak-giri <tilakgiri01@gmail.com>
2024-05-11 00:38:34 +0545

    interface

---
+++ interface/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MyNameSpace{
+    class Program{
+        static void Main(string[] args){
+            Car car = new Car();
+            Bike bike = new Bike();
+            Van van = new Van();
+            car.Go();
+            bike.Go();
+            van.Go();
+        }
+    }
+    interface ITwoWheeler{
+        void Go();  
+    }
+    interface IFourWheeler{
+        void Go();
+    }
+    class Car : IFourWheeler{
+        public void Go(){
+            Console.WriteLine("The vehicle is moving with 4 wheels.");
+        }
+    }
+    class Bike : ITwoWheeler{
+        public void Go(){
+        Console.WriteLine("The vehicle is moving with 2 wheels.");
+        }
+    }
+    class Van : IFourWheeler{
+        public void Go(){
+        Console.WriteLine("The vehicle is moving with 4 wheels.");
+        }
+    }
+}
