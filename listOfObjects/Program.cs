94d11bf760955352872dbd4e356bcc77000e419b
tilak-giri <tilakgiri01@gmail.com>
2024-05-11 23:20:28 +0545

    understanding list of objects in C#

---
+++ listOfObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyNameSpace{
+    class Program{
+        static void Main(string[] args){
+            List<Player> players= new List<Player>();
+
+            // Player player1 = new Player("Oggy");
+            // Player player2 = new Player("Jack");
+            // Player player3= new Player("Olivia");
+
+            players.Add(new Player("Oggy"));
+            players.Add(new Player("Jack"));
+            players.Add(new Player("Olivia"));
+
+            foreach(Player player in players){
+                Console.WriteLine(player);
+        }
+    }
+    class Player{
+        public string userName;
+        public Player(string userName){
+            this.userName = userName;
+        }
+        public override string ToString(){
+            return userName;
+        } 
+    }
+}
+}
