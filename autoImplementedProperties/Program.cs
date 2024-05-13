28835fda626de1dc178f7f012d29cc13591a5455
tilak-giri <tilakgiri01@gmail.com>
2024-05-13 23:15:10 +0545

    understanding auto implemented properties

---
+++ autoImplementedProperties/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Name{
+    class Program{
+        static void Main(string[] args){
+            Car car1 = new Car("mustang");
+            Console.WriteLine(car1.Model);
+        }
+    }
+    class Car{
+        public string Model{ get; set; }
+        public string model;
+        public Car(string model){
+            this.Model = model;
+        }
+    }
+}
