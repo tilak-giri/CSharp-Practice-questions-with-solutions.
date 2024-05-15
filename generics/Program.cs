362e401774c9396813740e11f4a4435a75630692
tilak-giri <tilakgiri01@gmail.com>
2024-05-15 23:24:27 +0545

    understanding generics in C#

---
+++ generics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Name{
+    class Program{
+        static void Main(string[] args){
+            int[] intArray = { 1, 2, 3 };
+            double[] doubleArray = { 1.0, 2.0, 3.0 };
+            string[] stringsArray = { "1", "2", "3" };
+
+            displayData(intArray);
+            displayData(doubleArray);
+            displayData(stringsArray);
+        }
+        public static void displayData<T>(T[] integer){
+            foreach (T i in integer){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
