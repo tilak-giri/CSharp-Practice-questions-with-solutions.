32e4339ed7e13fe932f4dcbe4b8f1e34b9d80309
tilak-giri <tilakgiri01@gmail.com>
2024-05-20 22:49:32 +0545

    understanding multithreading in C#

---
+++ multithreading/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MyNameSpace{
+    class Program{
+        static void Main(string[] args){
+            Thread thread1 = new Thread(CountDown);
+            Thread thread2 = new Thread(CountUp);
+            thread1.Start();
+            thread2.Start();
+        }
+        public static void CountUp(){
+            for (int i = 0; i<=10; i++){
+                Console.WriteLine($"Thread I counting : {i}");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("The countdown is over.");
+        }
+                public static void CountDown(){
+                for (int i = 10; i>=0; i--){
+                Console.WriteLine($"Thread II counting : {i}");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("The countdown is over.");
+        }
+    }
+}
